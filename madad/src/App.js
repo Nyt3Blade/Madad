f67616e98a36efc83cef510850a9d0327e91bf57
Yash <yashyadav942004@gmail.com>
2024-03-02 23:54:44 +0530

    .

--- madad/src/App.js
+++ madad/src/App.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Model from './Model';
+import Homepage from './Homepage';
+import Model2 from './Model2';
 
 function App() {
+
   return (
     <div>
-      <Model />
+      <Homepage />
     </div>
   )
 }
